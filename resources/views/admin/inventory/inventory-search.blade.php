4b88cde72f8c9abdc40f57ea4e41785519d4349e
tisuchi <tisuchi@gmail.com>
2018-02-19 14:08:05 +0800

    Fix photo continues loading

--- resources/views/admin/inventory/inventory-search.blade.php
+++ resources/views/admin/inventory/inventory-search.blade.php
@@ -589,6 +589,30 @@
 	);
 
 
+
+
+
+
+	/*for Carousel infinity loop*/
+	$('.carousel .item').each(function(){
+	  var next = $(this).next();
+	  if (!next.length) {
+	    next = $(this).siblings(':first');
+	  }
+	  next.children(':first-child').clone().appendTo($(this));
+
+	  if (next.next().length>0) {
+	 
+	      next.next().children(':first-child').clone().appendTo($(this)).addClass('rightest');
+	      
+	  }
+	  else {
+	      $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+	     
+	  }
+	});
+
+
 	
 
 
